47b70a2f309b59d769f69ccf4fea8d1c01629c85
Samuelgranato <samuelgranato87@gmail.com>
2019-03-12 01:36:36 -0300

    teste varias vezes e melhoras

--- images/Sign Up Form by Colorlib_arquivos/main.js
+++ images/Sign Up Form by Colorlib_arquivos/main.js
@@ -1,22 +1,5 @@
 (function($) {
 
-  $('#pergunta_6').parent().append('<ul class="list-item" id="newpergunta_6" name="pergunta_6"></ul>');
-  $('#pergunta_6 option').each(function(){
-      $('#newpergunta_6').append('<li value="' + $(this).val() + '">'+$(this).text()+'</li>');
-  });
-  $('#pergunta_6').remove();
-  $('#newpergunta_6').attr('id', 'pergunta_6');
-  $('#pergunta_6 li').first().addClass('init');
-  $("#pergunta_6").on("click", ".init", function() {
-      $(this).closest("#pergunta_6").children('li:not(.init)').toggle();
-  });
-  
-  var allOptions = $("#pergunta_6").children('li:not(.init)');
-  $("#pergunta_6").on("click", "li:not(.init)", function() {
-      allOptions.removeClass('selected');
-      $(this).addClass('selected');
-      $("#pergunta_6").children('.init').html($(this).html());
-      allOptions.toggle();
   });
   var slider = document.getElementById('co2_value');
   var marginSlider = document.getElementById('slider-margin');
